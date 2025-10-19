448881c62ae1ee4a2222f28299aacd8db7931f11
LuisDanielGodoy <113561775+ldanielgodoy@users.noreply.github.com>
2025-10-19 18:59:13 -0300

    tercer cambio

--- pre-entrega-automation-testing-Luis Daniel Godoy/test/test_saucedemo.py
+++ pre-entrega-automation-testing-Luis Daniel Godoy/test/test_saucedemo.py
@@ -1,36 +1,46 @@
 import pytest
+from selenium.webdriver.common.by import By
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
 
-
-
+from utils.helpers import login_saucedemo, get_driver
 
 @pytest.fixture
+def driver():
+    # configuracion para consultar a selenium web driver
+    driver = get_driver()
+    yield driver
+    driver.quit()
 
-def test_driver():
-    # Configuracion para cosultar a selenium web driver
-
-def test_login():
-    # assert
-
-    # logeo de usuario con username y password
+def test_login(driver ):
+    # Logeo de usuario con username y password
     # Click al boton de login
-    # Navegar a la página de login de saucedemo.com
-    # Ingresar credenciales válidas (usuario: "standard_user", contraseña: "secret_sauce")
-    # Validar login exitoso verificando que se haya redirigido a la página de inventario
-    # Verificar que el título de la página de inventario sea correcto (ventana)
+    login_saucedemo(driver)
+    assert "/inventory.html" in driver.current_url
+    titulo  = driver.find_element(By.CSS_SELECTOR, 'div.header_secondary_container .title').text
+    assert titulo == 'Products'
+    driver.save_screenshot('C:/Users/Luis Daniel Godoy/Desktop/pre-entrega-automation-testing-Luis Daniel Godoy/screenshots/test_login.png')
+    
+def test_catalago( driver ):
+    login_saucedemo( driver )
 
-def test_catalogo():
+    #verifixar el titulo de la pagina(ventanita)
+    products = driver.find_elements(By.CLASS_NAME, 'inventory_item')
+    assert len(products) > 0
+    driver.save_screenshot('C:/Users/Luis Daniel Godoy/Desktop/pre-entrega-automation-testing-Luis Daniel Godoy/screenshots/test_catalago.png')
 
-    # logeo de usuario con username y password
-    # Click al boton de login
-    # Verificar el titulo del htmal
-    # Comprobar que existan productos visibles en la página (al menos verificar la presencia de uno) mediante la funcion len()
-    # Validar que elementos importantes de la interfaz estén presentes (menú, filtros, etc.)
+def test_carrito( driver ):
+    login_saucedemo(driver)
+    products = driver.find_elements(By.CLASS_NAME, 'inventory_item')
+    assert len(products) > 0
 
-def test_carrito():
+    # verificar el titulo pero del html
+    products[0].find_element(By.TAG_NAME, 'button').click()
 
-    # logeo de usuario con username y password
-     # Click al boton de login
-    # Añadir un producto al carrito haciendo clic en el botón correspondiente
-    # Verificar que el contador del carrito se incremente correctamente
-    # Navegar al carrito de compras
-    # Comprobar que el producto añadido aparezca correctamente en el carrito
+    #Comprobar si existen productos en la pagina visibles (len())
+    #Verificar elementos importantes de la pagina.
+    badge = driver.find_element(By.CLASS_NAME, 'shopping_cart_badge').text
+    assert badge == "1" 
+    driver.save_screenshot('C:/Users/Luis Daniel Godoy/Desktop/pre-entrega-automation-testing-Luis Daniel Godoy/screenshots/test_carrito.png') 
+
