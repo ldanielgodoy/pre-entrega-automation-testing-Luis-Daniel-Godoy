eba0a038aaa66f3e81cdf35eb27d883b32f194db
Luis Daniel Godoy Lugo <Ldanielgodoy@gmail.com>
2025-10-19 18:12:43 -0300

    segundo cambio

---
+++ test/test_saucedemo.py
@@ -0,0 +1,46 @@
+import pytest
+from selenium.webdriver.common.by import By
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
+
+from utils.helpers import login_saucedemo, get_driver
+
+@pytest.fixture
+def driver():
+    # configuracion para consultar a selenium web driver
+    driver = get_driver()
+    yield driver
+    driver.quit()
+
+def test_login(driver ):
+    # Logeo de usuario con username y password
+    # Click al boton de login
+    login_saucedemo(driver)
+    assert "/inventory.html" in driver.current_url
+    titulo  = driver.find_element(By.CSS_SELECTOR, 'div.header_secondary_container .title').text
+    assert titulo == 'Products'
+    driver.save_screenshot('C:/Users/Luis Daniel Godoy/Desktop/pre-entrega-automation-testing-Luis Daniel Godoy/screenshots/test_login.png')
+    
+def test_catalago( driver ):
+    login_saucedemo( driver )
+
+    #verifixar el titulo de la pagina(ventanita)
+    products = driver.find_elements(By.CLASS_NAME, 'inventory_item')
+    assert len(products) > 0
+    driver.save_screenshot('C:/Users/Luis Daniel Godoy/Desktop/pre-entrega-automation-testing-Luis Daniel Godoy/screenshots/test_catalago.png')
+
+def test_carrito( driver ):
+    login_saucedemo(driver)
+    products = driver.find_elements(By.CLASS_NAME, 'inventory_item')
+    assert len(products) > 0
+
+    # verificar el titulo pero del html
+    products[0].find_element(By.TAG_NAME, 'button').click()
+
+    #Comprobar si existen productos en la pagina visibles (len())
+    #Verificar elementos importantes de la pagina.
+    badge = driver.find_element(By.CLASS_NAME, 'shopping_cart_badge').text
+    assert badge == "1" 
+    driver.save_screenshot('C:/Users/Luis Daniel Godoy/Desktop/pre-entrega-automation-testing-Luis Daniel Godoy/screenshots/test_carrito.png') 
+
---
+++ utils/helpers.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By #LLAMAR A LOS SELECTORES
+#from selenium.webdriver.common.keys import Keys #PASAR DATOS
+#from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+import time
+
+
+URL = 'https://www.saucedemo.com/'
+USERNAME = 'standard_user'
+PASSWORD = 'secret_sauce'
+
+def get_driver():
+
+    #options  = Options()
+    #options.add_argument('--start-maximized')
+
+    #instalacion de driver
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
+
+    time.sleep(7)
+
+    return driver
+
+def login_saucedemo( driver ):
+
+    driver.get(URL)
+    
+    #INGRESAR LAS CREDENCIALES
+    driver.find_element(By.NAME,'user-name').send_keys(USERNAME)
+    driver.find_element(By.NAME,'password').send_keys(PASSWORD)
+    driver.find_element(By.ID,'login-button').click()
+
+    time.sleep(10)
